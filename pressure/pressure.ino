702e604b83878bfbf4dadbdd29eebe9a3eb13374
Conor Doyle <18cldoyle@stbenilduscollege.com>
2024-03-10 22:40:06 +0000

    updated last minute

--- pressure/pressure.ino
+++ pressure/pressure.ino
@@ -1,32 +1,46 @@
-unsigned long i=0, time, timeStart;
+unsigned long i = 0, timeStart;
 float timeStamp;
-int pressureValue; 
-float pressure; 
+int pressureValue;
+float pressure;
 int sensorValue;
 float voltage;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(4, OUTPUT);pinMode(3, OUTPUT);  // only needed for this demo
-  digitalWrite(3,HIGH);//vcc
-  digitalWrite(4,LOW);//gnd
-  timeStart = micros();// start the internal clock
-  time = micros();// read time now
-  timeStamp = (time-timeStart)/1e6;
+  pinMode(4, OUTPUT);
+  pinMode(3, OUTPUT);
+  digitalWrite(3, HIGH);  //vcc
+  digitalWrite(4, LOW);   //gnd
+  timeStart = micros();   // start the internal clock
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  pressureValue = analogRead (A1);
-  sensorvalue=analogRead(A0);
-  volage= sensorVaule*(5.0/1023)
-  pressure = ((pressureValue / 1024.0) + 0.095) / 0.0009;
-  
-  Serial.print(timeStamp); Serial.print(",\t");
-  Serial.print(pressure); Serial.print(",\t");
-  Serial.print(temperature); Serial.print(",\t");
-  Serial.println(i); // counter for number of time we loop
-  delay(10); 
+  timeStart = micros();  // Reset the timeStart value
 
+  // Read sensor values
+  pressureValue = analogRead(A1);
+  sensorValue = analogRead(A0);
+
+  // Convert sensor value to voltage
+  voltage = sensorValue * (5.0 / 1023);
+
+  // Convert pressure value to pressure
+  pressure = ((pressureValue / 1024.0) + 0.095) / 0.0009;
+
+  // Calculate timestamp
+  timeStamp = (micros() - timeStart) / 1000000.0;
+
+  // Print values
+  Serial.print(timeStamp);
+  Serial.print(",\t");
+  Serial.print(pressure);
+  Serial.print(",\t");
+  Serial.print(voltage);   // send over serial 
+  Serial.print(",\t");
+  Serial.println(i);
+
+  // Increment loop counter
+  i++;
+
+  delay(10);  // Delay for stability
 }
